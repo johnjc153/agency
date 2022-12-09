8fb353b8ebf16458a78128277bb63a68e6cf8577
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2022-12-09 13:55:44 -0700

    assets for page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='agency__navbar'>
+      <div className="agency__navbar-links">
+        <div className="agency__navbar-links_log">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
   )
 }
 
