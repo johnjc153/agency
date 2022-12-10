350968f5d0ff4fc45d8021bc257822b0eadf3145
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2022-12-09 19:03:07 -0700

    header section frontend completed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 import './header.css'
 
+  
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='agency__header section__padding' id='home'>
+      <div className="agency__header-content">
+        
+        <h1 className='gradient__text'>Let's build something amazing together</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias dolorum soluta minima ad doloremque a reprehenderit tempore? Facere magnam recusandae rerum est. Laborum debitis saepe exercitationem libero quidem rem quaerat!</p>
+        
+        <div className="agency__header-content__input">
+          <input type="email" placeholder='Your email address' />
+          <button type='button' >Get Started</button>
+        </div>
+
+        <div className="agency__header-content__people">
+          <img src={people} alt="people" />
+          <p>1800 people have requested access</p>
+        </div>
+        
+      </div>
+
+      <div className="agency__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+    </div>
   )
 }
 
