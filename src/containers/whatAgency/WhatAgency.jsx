73de9b47d99aa6429a68c9b424d78052d1147efd
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2022-12-10 18:32:08 -0700

    what agency section css and setup

--- src/containers/whatAgency/WhatAgency.jsx
+++ src/containers/whatAgency/WhatAgency.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Feature } from '../../components'
 import './whatagency.css'
 
 const WhatAgency = () => {
   return (
-    <div>WhatAgency</div>
+    <div className="agency__whatagency section__margin" id='wwa'>
+      <div className="agency__whatagency-feature">
+        <Feature />
+      </div>
+
+      <div className="agency__whatagency-heading">
+        <h1 className="gradient__text">The possibilities are endless</h1>
+        <p>Explore the Library</p>
+      </div>
+
+      <div className="agency__whatagency-container">
+        <Feature />
+        <Feature />
+        <Feature />
+
+      </div>
+
+    </div>
   )
 }
 
