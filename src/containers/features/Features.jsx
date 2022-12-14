8d27a1976cceb10d928d8660439b740de5dd720e
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2022-12-13 18:24:52 -0700

    feautures structure and text

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './features.css'
 
-const Features = () => {
+const Features = ({ title, text}) => {
   return (
-    <div>Features</div>
+    <div className="agency__features-container__feature">
+      <div className="agency__features-container__feature-title">
+        <div />
+        <h1>{ title }</h1>
+      </div>
+
+      <div className="agency__features-container__feature-text">
+        <p>{ text }</p>
+      </div>
+
+    </div>
   )
 }
 
