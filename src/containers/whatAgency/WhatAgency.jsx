cbc85a28292b6c11238304c3d8ad4d9065f134bb
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2022-12-13 18:25:19 -0700

    integrated feature into went agency

--- src/containers/whatAgency/WhatAgency.jsx
+++ src/containers/whatAgency/WhatAgency.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import { Feature } from '../../components'
+// import { Feature } from '../../components'
+import Features from '../features/Features'
 import './whatagency.css'
 
 const WhatAgency = () => {
   return (
     <div className="agency__whatagency section__margin" id='wwa'>
       <div className="agency__whatagency-feature">
-        <Feature />
+        <Features title="What is agency" text="Lorem, ipsum dolor sit amet consectetur adipisicing elit."/>
       </div>
-
+      
       <div className="agency__whatagency-heading">
         <h1 className="gradient__text">The possibilities are endless</h1>
         <p>Explore the Library</p>
       </div>
 
       <div className="agency__whatagency-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        
+        <Features title="About us" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, corrupti." />
+        <Features title="Knowledgebase" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, corrupti." />
+        <Features title="Education" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, corrupti." />
 
       </div>
 
