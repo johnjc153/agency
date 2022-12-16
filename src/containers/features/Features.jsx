8cb6994e5a1cf68f070849f804ded547621b5061
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2022-12-15 18:14:47 -0700

    changed the feature container to component

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
 import './features.css'
 
-const Features = ({ title, text}) => {
+const Features = () => {
   return (
-    <div className="agency__features-container__feature">
-      <div className="agency__features-container__feature-title">
-        <div />
-        <h1>{ title }</h1>
-      </div>
-
-      <div className="agency__features-container__feature-text">
-        <p>{ text }</p>
-      </div>
-
-    </div>
+    <div>features</div>
   )
 }
 
