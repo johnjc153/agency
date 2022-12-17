98adfbfbda1f0e2bf3f5dccd639521e41b97fc14
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2022-12-17 16:35:28 -0700

    possibilities page

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './possibility.css'
+import possibilityImage from '../../assets/possibility.png'
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className="agency__possibility section__padding">
+      
+      <div className="agency__possibility-image">
+        <img src={ possibilityImage } alt="possibility" />
+      </div>
+
+      <div className="agency__possibility-content">
+        <h4>Get Started Today</h4>
+        <h1 className="gradient__text">Work with us</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum accusantium iure et ipsa aut placeat? Praesentium, excepturi sunt odio aliquid doloribus, accusamus eum eligendi laborum ex culpa in nulla itaque.</p>
+        <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, illum.</h4>
+      </div>
+
+    </div>
   )
 }
 
