37e6e8580b7f0157b53a56dfcc7bc57c7c0a1360
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2022-12-22 23:54:33 -0700

    cta completed

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,7 +3,17 @@ import './cta.css'
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className="agency__cta">
+      <div className="agency__cta-content">
+        <p>Request a quote</p>
+        <h3>Contact Us</h3>
+      </div>
+
+      <div className="agency__cta-btn">
+        <button type='button'>Get Started</button>
+      </div>
+
+    </div>
   )
 }
 
