c89b38247d1f48a076ec3ddd07ae060ff3cfdeb8
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2023-01-14 14:47:27 -0700

    possibility content added

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import './possibility.css'
-import possibilityImage from '../../assets/possibility.png'
+import webdesign from '../../assets/webdesign.png'
 
 const Possibility = () => {
   return (
     <div className="agency__possibility section__padding">
       
       <div className="agency__possibility-image">
-        <img src={ possibilityImage } alt="possibility" />
+        <img src={ webdesign } alt="possibility" />
       </div>
 
       <div className="agency__possibility-content">
         <h4>Get Started Today</h4>
         <h1 className="gradient__text">Work with us</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum accusantium iure et ipsa aut placeat? Praesentium, excepturi sunt odio aliquid doloribus, accusamus eum eligendi laborum ex culpa in nulla itaque.</p>
-        <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, illum.</h4>
+        <p>Ready to take your online presence to the next level? Contact us today for a free consultation and let's start building your dream website!</p>
+        <h4>You can also call, email, or visit our offices to get started.</h4>
       </div>
 
     </div>
