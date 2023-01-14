7c666efb4c3d530930430f0153284c4e12c6353e
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2023-01-14 14:49:03 -0700

    what agency section

--- src/containers/whatAgency/WhatAgency.jsx
+++ src/containers/whatAgency/WhatAgency.jsx
@@ -6,19 +6,19 @@ const WhatAgency = () => {
   return (
     <div className="agency__whatagency section__margin" id='wwa'>
       <div className="agency__whatagency-feature">
-        <Feature title="What is agency" text="Lorem, ipsum dolor sit amet consectetur adipisicing elit."/>
+        <Feature title="What is WebWorks?" text="We are a team of passionate designers and developers who are dedicated to helping businesses of all sizes succeed online. Our mission is to create websites that are not only visually stunning, but also functional and user-friendly."/>
       </div>
       
       <div className="agency__whatagency-heading">
-        <h1 className="gradient__text">The possibilities are endless</h1>
+        <h1 className="gradient__text">Here to establish your digital footprint</h1>
         <p>Explore the Library</p>
       </div>
 
       <div className="agency__whatagency-container">
         
-        <Feature title="About us" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, corrupti." />
-        <Feature title="Knowledgebase" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, corrupti." />
-        <Feature title="Education" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, corrupti." />
+        <Feature title="Assisting Every Step" text="We understand that building a website can be a daunting task, which is why we strive to make the process as smooth and stress-free as possible. Our team will work closely with you to understand your business goals and design a website that meets your unique needs." />
+        <Feature title="Constant Support" text="In addition to custom website development, we also offer ongoing support and maintenance to ensure your website stays up-to-date and running smoothly." />
+        <Feature title="Client Oriented" text="We pride ourselves on delivering top-notch customer service and results that speak." />
 
       </div>
 
