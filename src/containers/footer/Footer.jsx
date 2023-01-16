fade8e71ad59fbeef66a916b873b3583c1d20cff
johnjc153 <62703769+johnjc153@users.noreply.github.com>
2023-01-15 22:42:24 -0700

    footer section done with content

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
-import logo from '../../assets/logo.svg'
+import webworks from '../../assets/webworks.png'
 import './footer.css'
 
 const Footer = () => {
   return (
     <div className="agency__footer section__padding">
       <div className="agency__footer-heading">
-        <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+        <h1 className="gradient__text">Find Solutions, Contact Us</h1>
       </div>
 
       <div className="agency__footer-btn">
-        <p>Request Early Access</p>
+        <p>Contact Us For A Free Consultation</p>
       </div>
 
       <div className="agency__footer-links">
       
         <div className="agency__footer-links_logo">
-          <img src={logo} alt="agency_logo" />
-          <p>address <br /> All Rights Reserved</p>
+          <img src={webworks} alt="agency_logo" />
         </div>
       
         <div className="agency__footer-links_div">
           <h4>Links</h4>
-          <a href='#'>Overons</a>
+          <a href='#'>Who We Are</a>
           <a href='#'>Social Media</a>
           <a href='#'>Counters</a>
           <a href='#'>Contact</a>
@@ -44,7 +43,7 @@ const Footer = () => {
       </div>
 
       <div className="agency__footer-copyright">
-        <p>@2023 GPT-3. All rights reserved.</p>
+        <p>@2023 Webworks. All rights reserved.</p>
       </div>
     </div>
   )
